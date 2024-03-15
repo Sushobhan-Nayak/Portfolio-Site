e8dece4f90d2b93106c66429c7bcd3531f9c3061
Sushobhan Nayak <sushobhannayak17@gmail.com>
2024-03-15 22:47:47 +0530

    homepage started

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,30 @@
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row items-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* Image Container */}
+      <div className="h-1/2 relative flex justify-center lg:w-1/2 lg:h-full">
+        <img src="/hero.png" alt="" className="h-full object-contain"></img>
+      </div>
+      {/* Text Conatainer */}
+      <div className="h-1/2 flex flex-col gap-8 items-center justify-center lg:h-full lg:w-1/2">
+        {/* Title */}
+        <h1 className="font-bold text-4xl md:text-6xl w-full">
+          Crafting Digital Experiences, Designing Tomorrow
+        </h1>
+        {/* Descrp */}
+        <p className="md:text-xl w-full">Welcome to my portfolio website</p>
+        {/* Buttons */}
+        <div className="flex gap-4 w-full">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black">
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import NavLink from "./navLink";
 
 const Links = [
   { url: "/", title: "Home" },
@@ -13,16 +14,15 @@ const Links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div className="hidden md:flex gap-4">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* Links */}
+      <div className="hidden md:flex gap-4 w-1/3">
         {Links.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title}></NavLink>
         ))}
       </div>
       {/* Logo */}
-      <div className="md:hidden">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -33,9 +33,19 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div>
-        <Link href="https://github.com/Sushobhan-Nayak">
+      {/* Social Links */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="https://github.com/Sushobhan-Nayak" target="_blank">
           <img src="/github.png" alt="" width={24} height={24}></img>
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/sushobhan-nayak/"
+          target="_blank"
+        >
+          <img src="/linkedin.png" alt="" width={24} height={24}></img>
+        </Link>
+        <Link href="https://github.com/Sushobhan-Nayak" target="_blank">
+          <img src="/instagram.png" alt="" width={24} height={24}></img>
         </Link>
       </div>
       {/* Responsive menu */}
